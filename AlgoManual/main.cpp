4c53c615055956704adfff9ca444c095c022eb46
LSH <lsh@lshui-macbookpro.local>
2019-01-31 19:53:27 +0900

    Initial Commit

---
+++ AlgoManual/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoManual
+//
+//  Created by LSH on 31/01/2019.
+//  Copyright © 2019 LSH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
